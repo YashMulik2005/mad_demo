3d5d0a4a934666c79064ba1e22b271ba34459bb5
Shashank <you@example.com>
2023-08-14 20:02:49 +0530

    navigation to login and signup

--- app/src/main/java/com/example/demo/LoginActivity.java
+++ app/src/main/java/com/example/demo/LoginActivity.java
@@ -2,13 +2,25 @@ package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView setsignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setsignup= (TextView)findViewById(R.id.logintext);
+        setsignup.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent sn = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(sn);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/demo/SignupActivity.java
+++ app/src/main/java/com/example/demo/SignupActivity.java
@@ -2,13 +2,26 @@ package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
-
+    TextView setlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        setlogin=(TextView)findViewById(R.id.signuptext);
+        setlogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent lg = new Intent(SignupActivity.this,LoginActivity.class);
+                startActivity(lg);
+            }
+        });
     }
+
 }

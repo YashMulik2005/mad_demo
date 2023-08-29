46597a5ccc99a63944a2998afb61d9f54d51dc81
Shashank <you@example.com>
2023-08-29 16:43:08 +0530

    add done

--- app/src/main/java/com/example/demo/LoginActivity.java
+++ app/src/main/java/com/example/demo/LoginActivity.java
@@ -1,18 +1,26 @@
 package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    AppCompatButton im;
     TextView setsignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        im=(AppCompatButton)findViewById(R.id.loginbtn);
+       Animation scale = AnimationUtils.loadAnimation(this, R.anim.scaleanimation);
+       im.setAnimation(scale);
 
         setsignup= (TextView)findViewById(R.id.naviagtesignup);
         setsignup.setOnClickListener(new View.OnClickListener(){
--- app/src/main/java/com/example/demo/SignupActivity.java
+++ app/src/main/java/com/example/demo/SignupActivity.java
@@ -1,18 +1,26 @@
 package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
+    AppCompatButton im;
     TextView setlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        im=(AppCompatButton)findViewById(R.id.signupbtn);
+        Animation scale = AnimationUtils.loadAnimation(this, R.anim.scaleanimation);
+        im.setAnimation(scale);
 
         setlogin=(TextView)findViewById(R.id.naviagtelogin);
         setlogin.setOnClickListener(new View.OnClickListener(){
